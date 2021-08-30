b4aa90db652e88034ba135fca2c305bdb0321fff
deannno <dyno.wibowo@gmail.com>
2021-08-30 11:35:41 +1000

    fixed name in passporttest and add :any to res

--- tests/backend/passport.test.ts
+++ tests/backend/passport.test.ts
@@ -1,7 +1,8 @@
 /* Import the required types and libraries */
 import { agent } from "supertest";
 import app from "../../backend/src/server";
-import { compareSync } from "bcrypt";
+//import { compareSync } from "bcrypt";
+import { Response } from "express";
 
 /* Import the User and Name model */
 import { User, Name } from "../../backend/src/models/index";
@@ -15,15 +16,11 @@ const TEST_USER_EMAIL = "tony_pizza@gaming.com";
 
 describe(`Create account, login, logout, and delete`, () => {
 
-    const newNameField = {
-        first: TEST_USER_FIRST_NAME,
-        middle: TEST_USER_MID_NAME,
-        last: TEST_USER_LAST_NAME
-    }
-
     const newUserField = {
         email: TEST_USER_EMAIL,
-        name: newNameField,
+        firstName: TEST_USER_FIRST_NAME,
+        middleName: TEST_USER_MID_NAME,
+        lastName: TEST_USER_LAST_NAME,
         password: TEST_USER_PASSWORD
     };
 
@@ -36,8 +33,9 @@ describe(`Create account, login, logout, and delete`, () => {
         await userAgent
             .post('/api/passport/register')
             .send(newUserField)
-            .then((res) => {
-                expect(res.text).toBe('SUCCESS');
+            .then((res: any) => {
+                // expect(res.headersSent).toBeTruthy();
+                expect(res.text).toBe("SUCCESS");
             })
     });
 
@@ -50,7 +48,8 @@ describe(`Create account, login, logout, and delete`, () => {
         await userAgent
             .post('/api/passport/login')
             .send(req)
-            .then((res) => {
+            .then((res: any) => {
+                // expect(res.headersSent).toBeTruthy();
                 expect(res.text).toBe("SUCCESS");
             })
     });
@@ -59,7 +58,8 @@ describe(`Create account, login, logout, and delete`, () => {
     test('Logout', async () => {
         await userAgent
             .get('/api/passport/logout')
-            .then((res) => {
+            .then((res: any) => {
+                // expect(res.headersSent).toBeTruthy();
                 expect(res.text).toBe("SUCCESS");
             })
     })
