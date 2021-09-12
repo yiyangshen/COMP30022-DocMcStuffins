36fa464437c62311b20f3ebf3bbb606ccdfd5c84
Livya Natasha Riany <livyanatasha.riany@student.unimelb.edu.au>
2021-09-12 18:20:49 +0700

    Added top navigation bar and search bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 /* Import the required libraries and types */
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Router, Route, Switch } from "react-router-dom";
 // import axios from "axios";
-import './App.css';
+import "./App.css";
 
 /* Import components */
 import history from "./history";
@@ -10,32 +11,31 @@ import Nav from "./components/nav";
 
 /* Component to create routes */
 function App() {
-  return (
-    <div>
-      <Router history = {history}>
-        <Switch>
-          <Route exact path="/login" />
-          <Route exact path="/signup" />
+    return (
+        <div>
+            <Router history={history}>
+                <Switch>
+                    <Route exact path="/login" />
+                    <Route exact path="/signup" />
 
-          <div>
-            <Nav />
-            <Route exact path="/dashboard" />
-            <Route exact path="/profile" />
+                    <div>
+                        <Nav />
+                        <Route exact path="/dashboard" />
+                        <Route exact path="/profile" />
 
-            <Route exact path="/contacts" />
-            <Route exact path="/contacts/add" />
+                        <Route exact path="/contacts" />
+                        <Route exact path="/contacts/add" />
 
-            <Route exact path="/groups" />
-            <Route exact path="/groups/add" />
+                        <Route exact path="/groups" />
+                        <Route exact path="/groups/add" />
 
-            <Route exact path="/memos" />
-            <Route exact path="/memos/add" />
-          </div>
-          
-        </Switch>
-      </Router>
-    </div>
-  );
+                        <Route exact path="/memos" />
+                        <Route exact path="/memos/add" />
+                    </div>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/components/nav.tsx
+++ frontend/src/components/nav.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { ReactSVG } from "react-svg";
+import { Nav, Navbar, Form } from "react-bootstrap";
 
 /* Import components */
 import "../css/nav.css";
@@ -13,45 +14,62 @@ import Memos from "../img/memos.svg";
 import Logout from "../img/logout.svg";
 
 /* Navigation bar at the bottom of all customer page */
-export default function Nav() {
+export default function Navi() {
     return (
-        <nav className="sidenav">
-            <img className="logo" src={Logo} alt="logo" />
+        <div>
+            <nav className="sidenav">
+                <img className="logo" src={Logo} alt="logo" />
 
-            <NavLink className="contents" exact to="/dashboard">
-                <h2>
-                    <ReactSVG className="icon" src={Dashboard} />
-                    Dashboard
-                </h2>
-            </NavLink>
+                <NavLink className="contents" exact to="/dashboard">
+                    <h2>
+                        <ReactSVG className="icon" src={Dashboard} />
+                        Dashboard
+                    </h2>
+                </NavLink>
 
-            <NavLink className="contents" exact to="/contacts">
-                <h2>
-                    <ReactSVG class="icon" src={Contacts} />
-                    Contacts
-                </h2>
-            </NavLink>
+                <NavLink className="contents" exact to="/contacts">
+                    <h2>
+                        <ReactSVG class="icon" src={Contacts} />
+                        Contacts
+                    </h2>
+                </NavLink>
 
-            <NavLink className="contents" exact to="/groups">
-                <h2>
-                    <ReactSVG class="icon" src={Groups} />
-                    Groups
-                </h2>
-            </NavLink>
+                <NavLink className="contents" exact to="/groups">
+                    <h2>
+                        <ReactSVG class="icon" src={Groups} />
+                        Groups
+                    </h2>
+                </NavLink>
 
-            <NavLink className="contents" exact to="/memos">
-                <h2>
-                    <ReactSVG class="icon" src={Memos} />
-                    Memos
-                </h2>
-            </NavLink>
+                <NavLink className="contents" exact to="/memos">
+                    <h2>
+                        <ReactSVG class="icon" src={Memos} />
+                        Memos
+                    </h2>
+                </NavLink>
 
-            <NavLink className="contents logout" exact to="/login">
-                <h2>
-                    <ReactSVG class="icon" src={Logout} />
-                    Logout
-                </h2>
-            </NavLink>
-        </nav>
+                <NavLink className="contents logout" exact to="/login">
+                    <h2>
+                        <ReactSVG class="icon" src={Logout} />
+                        Logout
+                    </h2>
+                </NavLink>
+            </nav>
+
+            <Navbar expand="lg" className="d-flex flex-row-reverse p-2 header">
+                <Nav>
+                    <Nav.Item>
+                        <Nav.Link href="/profile">
+                            <h2>Profile</h2>
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                <Form className="form-center">
+                    <h2>
+                        <input type="text" placeholder="Search" className="" />
+                    </h2>
+                </Form>
+            </Navbar>
+        </div>
     );
 }
