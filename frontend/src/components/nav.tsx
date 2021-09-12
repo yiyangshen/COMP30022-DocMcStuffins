6d24061f7f87de301a014c0cf9135d1286a2c9f9
Livya Natasha Riany <livyanatasha.riany@student.unimelb.edu.au>
2021-09-12 14:39:49 +0700

    Added navigation bar

--- frontend/src/components/nav.tsx
+++ frontend/src/components/nav.tsx
@@ -1,28 +1,29 @@
 /* Import the required libraries and types */
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 /* Import components */
+import "../css/nav.css";
 
 /* Navigation bar at the bottom of all customer page */
 export default function Nav() {
     return (
-        <nav>
-            <NavLink exact to="/dashboard">
-
+        <nav className="sidenav">
+            <NavLink className="sidenav a" exact to="/dashboard">
+                <h2>Dashboard</h2>
             </NavLink>
 
             <NavLink exact to="/contacts">
-                
+                <h2>Contacts</h2>
             </NavLink>
 
             <NavLink exact to="/groups">
-                
+                <h2>Groups</h2>
             </NavLink>
 
             <NavLink exact to="/memos">
-                
+                <h2>Memos</h2>
             </NavLink>
         </nav>
-    )
-};
+    );
+}
