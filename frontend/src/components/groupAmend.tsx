97fbf3061c4934f7cd142b5b06c07781f28e9bb7
Souajh <jsituwali@student.unimelb.edu.au>
2021-10-01 14:05:29 +1000

    base case

--- frontend/src/components/groupAmend.tsx
+++ frontend/src/components/groupAmend.tsx
@@ -1,19 +1,196 @@
+// /* Import the required libraries and types */
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import history from "../history";
+// import "../css/newGroup.css";
+
+// /* Import the required libraries and types */
+// import { amendGroupDetails, getGroupDetails } from "../api/groupApi";
+// import { IContact } from "../interfaces";
+// import { getId } from "../api/userApi";
+// import { getContactDetails } from "../api/contactApi";
+
+// class newGroup extends React.Component {
+//     state = {
+//         isLoaded: false,
+//         name: "",
+//         members: "" as any,
+//         contactsList: [] as IContact[],
+//     };
+
+//     /* Set state accordingly to the target */
+//     handleChange = (event: { target: { name: any; value: String } }) => {
+//         this.setState({ [event.target.name]: event.target.value });
+//     };
+
+//     groupId = getId() || "";
+
+//     /* During loading page */
+//     async componentDidMount() {
+        // getGroupDetails(this.groupId).then(
+        //     (response) => {
+        //         var data = response.data.data;
+        //         console.log(data);
+        //         this.setState({
+        //             isLoaded: true,
+        //             contactsList: data.members, //in terms of Icontacts[]
+        //             name: data.name,
+        //         });
+        //     },
+        //     (error) => {
+        //         this.setState({ isLoaded: true, error });
+        //         console.log(error);
+        //     }
+        // );
+//         /* Retrieve item in local storage and parse them */
+//         const value = localStorage.getItem("chosen");
+//         if (value) {
+//             this.setState({ members: JSON.parse(value) });
+//         }
+//         const label = localStorage.getItem("name");
+//         if (label) {
+//             this.setState({ name: JSON.parse(label) });
+//         }
+        
+//         const { members } = this.state;
+//         /* Loop through all contact ids and get their details, set the state */
+//         for (var i = 0; i < members.length; i++) {
+//             getContactDetails(members[i]).then(
+//                 (response) => {
+//                     var data = response.data.data;
+//                     this.setState({
+//                         contactsList: [this.state.contactsList, data],
+//                     });
+//                     console.log(response);
+//                 },
+//                 (error) => {
+//                     console.log(error);
+//                 }
+//             );
+//         }
+//     }
+//     /* Remember state for the next mount */
+//     componentWillUnmount() {
+//         localStorage.setItem("name", JSON.stringify(this.state.name));
+//     }
+
+//     /* Handle when click on submit button */
+//     handleSubmit = (event: { preventDefault: () => void }) => {
+//         const { name, members,} = this.state;
+//         event.preventDefault();
+//         localStorage.removeItem("name");
+//         localStorage.removeItem("chosen");
+//         amendGroupDetails(this.groupId, name, members);
+//     };
+
+//     /* Handle when click on cancel button */
+//     handleCancel = (event: { preventDefault: () => void }) => {
+//         localStorage.removeItem("name");
+//         localStorage.removeItem("chosen");
+//         history.push("/groups");
+//     };
+
+//     render() {
+//         const { name, contactsList, members } = this.state;
+
+//         return (
+//             <div className="border">
+//                 <h1>Edit Group</h1>
+//                 <div className="AGbox">
+//                     <label>Name</label>
+//                     <input
+//                         type="name"
+//                         id="groupName"
+//                         name="name"
+//                         placeholder="Eg. Unimelb"
+//                         value={name}
+//                         onChange={this.handleChange}
+//                     />
+//                     <div className="box1">
+//                         <label>Members</label>
+//                         <div className="box, white">
+//                             <h2>{members.length + contactsList.length}</h2>
+//                         </div>
+//                         <Link to="/groups/new/contact" className="addContact">
+//                             add contact
+//                         </Link>
+//                     </div>
+//                 </div>
+
+//                 <table>
+//                     <thead>
+//                         <tr className="table-lable">
+//                             <th>Name</th>
+//                             <th>Phone</th>
+//                             <th>Email</th>
+//                         </tr>
+//                     </thead>
+//                     {contactsList !== undefined && contactsList.length > 0 ? (
+//                         <div>
+//                             {contactsList.map((contact, i) => (
+//                                 <div key={i}>
+//                                     {" "}
+//                                     <tbody>
+//                                         <tr className="table-contents">
+//                                             <td>
+//                                                 {contact.name.first}{" "}
+//                                                 {contact.name.last}
+//                                             </td>
+//                                             <td>{contact.phoneNumber}</td>
+//                                             <td>{contact.email}</td>
+//                                         </tr>
+//                                     </tbody>
+//                                 </div>
+//                             ))}{" "}
+//                         </div>
+//                     ) : (
+//                         <tbody>
+//                             <tr>
+//                                 <td></td>
+//                                 <td>No data yet</td>
+//                                 <td></td>
+//                             </tr>
+//                         </tbody>
+//                     )}
+//                 </table>
+//                 <button
+//                     className="base-button"
+//                     type="button"
+//                     onClick={this.handleCancel}
+//                 >
+//                     <h2>Cancel</h2>
+//                 </button>
+//                 <button
+//                     className="base-button"
+//                     type="submit"
+//                     onClick={this.handleSubmit}
+//                 >
+//                     <h2>Save</h2>
+//                 </button>
+//             </div>
+//         );
+//     }
+// }
+
+// export default newGroup;
+
+
+
 /* Import the required libraries and types */
 import React from "react";
 import { Link } from "react-router-dom";
 import history from "../history";
+import { IContact } from "../interfaces";
+
+/* Import components */
+import { createGroup } from "../api/groupApi";
+import { getContactDetails } from "../api/contactApi";
 import "../css/newGroup.css";
 
-/* Import the required libraries and types */
-import { amendGroupDetails, createGroup, deleteGroup, getGroupDetails } from "../api/groupApi";
-// import { getContactDetails } from "../api/contactApi";
-import { IContact } from "../interfaces";
-import { getId } from "../api/userApi";
-import { getContactDetails } from "../api/contactApi";
-
-class newGroup extends React.Component {
+/* Component for new cgroup */
+class groupNew extends React.Component {
+    /* Declare states */
     state = {
-        isLoaded: false,
         name: "",
         members: "" as any,
         contactsList: [] as IContact[],
@@ -23,8 +200,6 @@ class newGroup extends React.Component {
     handleChange = (event: { target: { name: any; value: String } }) => {
         this.setState({ [event.target.name]: event.target.value });
     };
-
-    groupId = getId() || "";
 
     /* During loading page */
     async componentDidMount() {
@@ -37,69 +212,57 @@ class newGroup extends React.Component {
         if (label) {
             this.setState({ name: JSON.parse(label) });
         }
-        if(this.state.name){
-            amendGroupDetails(this.groupId, this.state.name, this.state.members);
-        }
-        
 
-        getGroupDetails(this.groupId).then(
-            (response) => {
-                var data = response.data.data;
-                console.log(data);
-                this.setState({
-                    isLoaded: true,
-                    contactsList: data.members,
-                    name: data.name,
-                });
-            },
-            (error) => {
-                this.setState({ isLoaded: true, error });
-                console.log(error);
-            }
-        );
-        // const { members } = this.state;
+        const { members } = this.state;
+
         /* Loop through all contact ids and get their details, set the state */
-        // for (var i = 0; i < members.length; i++) {
-        //     getContactDetails(members[i]).then(
-        //         (response) => {
-        //             var data = response.data.data;
-        //             this.setState({
-        //                 contactsList: [this.state.contactsList, data],
-        //             });
-        //             console.log(response);
-        //         },
-        //         (error) => {
-        //             console.log(error);
-        //         }
-        //     );
-        // }
-        
+        for (var i = 0; i < members.length; i++) {
+            getContactDetails(members[i]).then(
+                (response) => {
+                    var data = response.data.data;
+                    this.setState({
+                        contactsList: [...this.state.contactsList, data],
+                    });
+                    console.log(response);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+        }
     }
 
+    /* Remember state for the next mount */
+    componentWillUnmount() {
+        localStorage.setItem("name", JSON.stringify(this.state.name));
+    }
 
     /* Handle when click on submit button */
     handleSubmit = (event: { preventDefault: () => void }) => {
-        const { name, members, contactsList } = this.state;
+        const { name, members } = this.state;
         event.preventDefault();
+
+        /* Remove item from local storage and create group */
         localStorage.removeItem("name");
         localStorage.removeItem("chosen");
-        deleteGroup(this.groupId);
         createGroup(name, members);
     };
 
     /* Handle when click on cancel button */
     handleCancel = (event: { preventDefault: () => void }) => {
+        /* Remove item from local storage and push to group page */
         localStorage.removeItem("name");
         localStorage.removeItem("chosen");
         history.push("/groups");
     };
 
+    /* Render the component to the screen */
     render() {
         const { name, contactsList, members } = this.state;
 
         return (
             <div className="border">
-                <h1>Edit Group</h1>
+                <h1>Add Group</h1>
                 <div className="AGbox">
                     <label>Name</label>
                     <input
@@ -113,7 +276,7 @@ class newGroup extends React.Component {
                     <div className="box1">
                         <label>Members</label>
                         <div className="box, white">
-                            <h2>{contactsList.length}</h2>
+                            <h2>{members.length}</h2>
                         </div>
                         <Link to="/groups/new/contact" className="addContact">
                             add contact
@@ -145,7 +308,7 @@ class newGroup extends React.Component {
                                         </tr>
                                     </tbody>
                                 </div>
-                            ))}{" "}
+                            ))}
                         </div>
                     ) : (
                         <tbody>
@@ -169,11 +332,12 @@ class newGroup extends React.Component {
                     type="submit"
                     onClick={this.handleSubmit}
                 >
-                    <h2>Save</h2>
+                    <h2>Submit</h2>
                 </button>
             </div>
         );
     }
 }
 
-export default newGroup;
+/* Export component */
+export default groupNew;
