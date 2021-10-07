b02f0f0faafe2bda7e0f8f8844340a0d54ceb062
deannno <dyno.wibowo@gmail.com>
2021-10-08 08:15:27 +1100

    made changes to getGrouplessContacts test to fit requested structure

--- backend/tests/controllers/contacts/getGrouplessContacts.test.ts
+++ backend/tests/controllers/contacts/getGrouplessContacts.test.ts
@@ -6,33 +6,56 @@ import app from "../../../src/config/serverConfig";
 /* Import models */
 import { User, Contact, Group } from "../../../src/models/index";
 
-const TEST_USER_EMAIL_1 = "phil@gaming.comt"; 
+const TEST_USER_EMAIL_1 = "phil@gaming.com"; 
 const TEST_USER_EMAIL_2 = TEST_USER_EMAIL_1 + "1"; 
 const TEST_USER_FIRST_NAME = "Philip";
 const TEST_USER_LAST_NAME = "Holes";
 const TEST_USER_PASSWORD = "phillycheese";
 
-const TEST_GROUP_NAME = "Pork Belly!";
-const TEST_GROUP_NAME_2 = "Katsu";
+const TEST_USER_1 = {
+    email: "phil@gaming.comt",
+    firstName : "Philip",
+    lastName : "Holes",
+    password : "phillycheese"
+}
 
-const TEST_CONTACT_FIRST_NAME_GROUPED = "Adam";
-const TEST_CONTACT_LAST_NAME_GROUPED = "Smithy";
-const TEST_CONTACT_GENDER_GROUPED = "Female";
-const TEST_CONTACT_DATE_CREATED_GROUPED = new Date("1/1/2021");
-const TEST_CONTACT_DATE_VIEWED_GROUPED = new Date();
+const TEST_USER_2 = {
+    email: TEST_USER_1.email + ".au",
+    firstName: TEST_USER_1.firstName,
+    lastName: TEST_USER_1.lastName,
+    password: TEST_USER_1.password
+}
 
-const TEST_CONTACT_FIRST_NAME_GROUPED_2 = "Bald";
-const TEST_CONTACT_LAST_NAME_GROUPED_2 = "Bankrupt";
-const TEST_CONTACT_GENDER_GROUPED_2 = "Other";
-const TEST_CONTACT_DATE_CREATED_GROUPED_2 = new Date("11/1/2021");
-const TEST_CONTACT_DATE_VIEWED_GROUPED_2 = new Date();
+const TEST_GROUP_1 = {
+    name : "Pork Belly"
+}
+const TEST_GROUP_2 = {
+    name : "Katsu Curry"
+}
 
-const TEST_CONTACT_FIRST_NAME_UNGROUPED = "Ricardo";
-const TEST_CONTACT_LAST_NAME_UNGROUPED = "Tempesto";
-const TEST_CONTACT_GENDER_UNGROUPED = "Male";
-const TEST_CONTACT_DATE_CREATED_UNGROUPED = new Date("3/1/2021");
-const TEST_CONTACT_DATE_VIEWED_UNGROUPED = new Date();
+const TEST_CONTACT_GROUPED_1 = {
+    firstName : "Adam",
+    lastName : "Smith",
+    gender : "Female",
+    dateCreated : new Date("1/1/2021"),
+    dateViewed: new Date()
+}
 
+const TEST_CONTACT_GROUPED_2 = {
+    firstName : "Bald",
+    lastName : "Bankrupt",
+    gender : "Other",
+    dateCreated : new Date("11/1/2021"),
+    dateViewed : new Date()
+}
+
+const TEST_CONTACT_UNGROUPED = {
+    firstName : "Ricardo",
+    lastName : "Tempesto",
+    gender : "Male",
+    dateCreated : new Date("3/1/2021"),
+    dateViewed : new Date()
+}
 
 const BASE_URL = "/api/contacts/groupless";
 
@@ -41,21 +64,21 @@ describe('Contacts lists (getContacts)', () => {
         // Register new users for testing
         // user with grouped and ungrouped contact
         const newUser1 = new User({
-            email: TEST_USER_EMAIL_1,
-            password: TEST_USER_PASSWORD,
+            email: TEST_USER_1.email,
+            password: TEST_USER_1.password,
             name: {
-                first: TEST_USER_FIRST_NAME,
-                last: TEST_USER_LAST_NAME
+                first: TEST_USER_1.firstName,
+                last: TEST_USER_1.lastName
             }
         });
 
         // user with only grouped contact
         const newUser2 = new User({
-            email: TEST_USER_EMAIL_2,
-            password: TEST_USER_PASSWORD,
+            email: TEST_USER_2.email,
+            password: TEST_USER_2.password,
             name: {
-                first: TEST_USER_FIRST_NAME,
-                last: TEST_USER_LAST_NAME
+                first: TEST_USER_2.firstName,
+                last: TEST_USER_2.lastName
             }
         });
 
@@ -65,11 +88,11 @@ describe('Contacts lists (getContacts)', () => {
         // add new groups
         const newGroup = new Group({
             userId: newUser1._id,
-            name : TEST_GROUP_NAME
+            name : TEST_GROUP_1.name
         })
         const newGroup2 = new Group({
             userId: newUser2._id,
-            name : TEST_GROUP_NAME_2
+            name : TEST_GROUP_2.name
         })
 
         await newGroup.save();
@@ -79,26 +102,26 @@ describe('Contacts lists (getContacts)', () => {
         const newContactGrouped = new Contact({
             userId : newUser1._id,
             name : {
-                first: TEST_CONTACT_FIRST_NAME_GROUPED,
-                last: TEST_CONTACT_LAST_NAME_GROUPED
+                first: TEST_CONTACT_GROUPED_1.firstName,
+                last: TEST_CONTACT_GROUPED_1.lastName
             },
-            gender: TEST_CONTACT_GENDER_GROUPED,
+            gender: TEST_CONTACT_GROUPED_1.gender,
             timestamps:{
-                created:TEST_CONTACT_DATE_CREATED_GROUPED,
-                viewed:TEST_CONTACT_DATE_VIEWED_GROUPED
+                created: TEST_CONTACT_GROUPED_1.dateCreated,
+                viewed: TEST_CONTACT_GROUPED_1.dateViewed
             },
             groupId : newGroup._id
         })
         const newContactUngrouped = new Contact({
             userId : newUser1._id,
             name : {
-                first: TEST_CONTACT_FIRST_NAME_UNGROUPED,
-                last: TEST_CONTACT_LAST_NAME_UNGROUPED
+                first: TEST_CONTACT_UNGROUPED.firstName,
+                last: TEST_CONTACT_UNGROUPED.lastName
             },
-            gender: TEST_CONTACT_GENDER_UNGROUPED,
+            gender: TEST_CONTACT_UNGROUPED.gender,
             timestamps:{
-                created:TEST_CONTACT_DATE_CREATED_UNGROUPED,
-                viewed:TEST_CONTACT_DATE_VIEWED_UNGROUPED
+                created: TEST_CONTACT_UNGROUPED.dateCreated,
+                viewed: TEST_CONTACT_UNGROUPED.dateViewed
             }
         })
 
@@ -109,13 +132,13 @@ describe('Contacts lists (getContacts)', () => {
         const newContactGrouped2 = new Contact({
             userId : newUser1._id,
             name : {
-                first: TEST_CONTACT_FIRST_NAME_GROUPED_2,
-                last: TEST_CONTACT_LAST_NAME_GROUPED_2
+                first: TEST_CONTACT_GROUPED_2.firstName,
+                last: TEST_CONTACT_GROUPED_2.lastName
             },
-            gender: TEST_CONTACT_GENDER_GROUPED_2,
+            gender: TEST_CONTACT_GROUPED_2.gender,
             timestamps:{
-                created:TEST_CONTACT_DATE_CREATED_GROUPED_2,
-                viewed:TEST_CONTACT_DATE_VIEWED_GROUPED_2
+                created: TEST_CONTACT_GROUPED_2.dateCreated,
+                viewed: TEST_CONTACT_GROUPED_2.dateViewed,
             },
             groupId : newGroup2._id
         })
@@ -136,8 +159,8 @@ describe('Contacts lists (getContacts)', () => {
 
     test('2. Get lists of ungrouped contacts of an authenticated user', async () => {
         const req: any = {
-            email: TEST_USER_EMAIL_1,
-            password: TEST_USER_PASSWORD
+            email: TEST_USER_1.email,
+            password: TEST_USER_1.password
         };
 
         await userAgent
@@ -154,16 +177,16 @@ describe('Contacts lists (getContacts)', () => {
                 console.log(res.body.data);
                 // confirming contacts
                 expect(res.body.data.length).toEqual(1);
-                expect(res.body.data[0].name.first).toBe(TEST_CONTACT_FIRST_NAME_UNGROUPED);
-                expect(res.body.data[0].name.last).toBe(TEST_CONTACT_LAST_NAME_UNGROUPED);
+                expect(res.body.data[0].name.first).toBe(TEST_CONTACT_UNGROUPED.firstName);
+                expect(res.body.data[0].name.last).toBe(TEST_CONTACT_UNGROUPED.lastName);
             })
         await userAgent.get(`/api/user/logout`);
     });
 
     test('3. Get contact list of user with 0 ungrouped contacts', async () => {
         const req: any = {
-            email: TEST_USER_EMAIL_2,
-            password: TEST_USER_PASSWORD
+            email: TEST_USER_2.email,
+            password: TEST_USER_2.password
         };
 
         await userAgent
