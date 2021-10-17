51c77b372e7db25e7d6bf9df0158158a8b30cfef
Souajh <jsituwali@student.unimelb.edu.au>
2021-10-17 21:13:02 +1100

    no gay logo

--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -1,31 +1,35 @@
 /* Import the required libraries and types */
 import React from "react";
 import history from "../history";
-
+import WelcomeLogo from "../img/Welcome.svg";
+import Gay from "../img/Gay.svg";
 /* Component for home */
 class home extends React.Component {
     /* Render the component to the screen */
     render() {
         return (
-            <div className="frame-pages">
-                <h1>Welcome to Comp 30023</h1>
-                <br />
-                <span className="landingbtn">
-                    <button
-                        className="base-button"
-                        type="button"
-                        onClick={() => history.push(`/signin`)}
-                    >
-                        <h2>Sign In</h2>
-                    </button>
-                    <button
-                        className="base-button"
-                        type="button"
-                        onClick={() => history.push(`/register`)}
-                    >
-                        <h2>Sign Up</h2>
-                    </button>
-                </span>
+            <div className="center-pages">
+                <div className="WelcomeBox">
+                    <h1>Welcome to Futaba's Friday</h1>
+                    <img className="welcomeLogo" src={WelcomeLogo} alt="logo" />
+                    <div className="buttons">
+                        <button
+                            className="base-button"
+                            type="button"
+                            onClick={() => history.push(`/signin`)}
+                        >
+                            <h2>Sign In</h2>
+                        </button>
+                        <button
+                            className="base-button"
+                            type="button"
+                            onClick={() => history.push(`/register`)}
+                        >
+                            <h2>Sign Up</h2>
+                        </button>
+                    </div>
+                </div>
+                
             </div>
         );
     }
